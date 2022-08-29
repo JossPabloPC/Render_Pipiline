9ac17657a5e440c796dce1d0b70a8413718d930e
JossPabloPC <51423507+JossPabloPC@users.noreply.github.com>
2022-08-29 13:57:17 -0500

    tkinter implemented

--- app.py
+++ app.py
@@ -1,26 +1,30 @@
-from flask import Flask
-from flask import render_template
-from flask import Response
-import cv2 as cv
+# Import required Libraries
+from tkinter import *
+from PIL import Image, ImageTk
 import numpy as np
+import cv2
 
-import Coordinates 
+# Create an instance of TKinter Window or frame
+win= Tk()
 
-app = Flask(__name__) #Inicializa la app
+# Set the size of the window
+win.geometry("700x350")# Create a Label to capture the Video frames
+label =Label(win)
+label.grid(row=0, column=0)
+cap= np.zeros((300,300,3), dtype=np.uint8)
 
+# Define function to show frame
+def show_frames():
+      # Get the latest frame and convert into Image
+      
+      img = Image.fromarray(cap)
 
+      # Convert image to PhotoImage
+      imgtk = ImageTk.PhotoImage(image = img)
+      label.imgtk = imgtk
+      label.configure(image=imgtk)
 
-@app.route("/")
-def index():
-    cameraPos = Coordinates.Vector3(1, 1, 1)
+# Repeat after an interval to capture continiously
 
-    img = np.zeros([50,500,3], dtype=np.uint8)
-    img[cameraPos.x, cameraPos.y] = 255
-    
-    cv.imwrite("PipeRenderer/Render_Pipiline/static/image.png", img)
-    return render_template("index.html")
-
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+show_frames()
+win.mainloop()
