d430fc9d923ef0b0383dc6c3eeea4ff84ef22f91
JossPabloPC <51423507+JossPabloPC@users.noreply.github.com>
2022-08-29 20:32:25 -0500

    magnitud añadida al vector

--- Coordinates.py
+++ Coordinates.py
@@ -1,5 +1,10 @@
+import math
+
 class Vector3:
     def __init__(self, x,y,z):
         self.x = x
         self.y = y
         self.z = z
+
+    def magnitud():
+       self.magnitud = math.sqrt(math.pow(x, 2) + math.pow(y, 2) + math.pow(z, 2))
--- app.py
+++ app.py
@@ -6,15 +6,13 @@ import cv2
 
 #Functions
 def array2Window(arr):
-      mylabel = tk.Label(text="clicked")
-      mylabel.pack()     
-#      img = Image.fromarray(arr)
-#
-#      # Convert image to PhotoImage
-#      imgtk             = ImageTk.PhotoImage(image = img)
-#      output_image      = tk.Label(image=imgtk)
-#      output_image.image= imgtk
-#      output_image.pack()
+      img = Image.fromarray(arr)
+
+      # Convert image to PhotoImage
+      imgtk             = ImageTk.PhotoImage(image = img)
+      output_image      = tk.Label(image=imgtk)
+      output_image.image= imgtk
+      output_image.pack()
 
 # Create an instance of TKinter Window or frame
 win= tk.Tk()
@@ -28,18 +26,21 @@ coordenadas = tk.Frame()
 
 #Widgets
 greeting = tk.Label(text="Welcome to Python Render Pipeline", fg="white", bg="black",width=50, height=3)
-entryX      = tk.Entry(master=coordenadas)
-entryY       = tk.Entry(master=coordenadas)
-run_btn = tk.Button(command=array2Window(cap), text= "Run")
+
+#Camara
+Camera_X_entry = tk.Entry(master=coordenadas)
+Camera_Y_entry = tk.Entry(master=coordenadas)
+Camera_Z_entry = tk.Entry(master=coordenadas)
+
+#Punto
+
+run_btn     = tk.Button(command=lambda: array2Window(arr=cap), text= "Run")
 
 #Packing
 greeting.pack()
 entryX.pack(side=tk.LEFT)
 entryY.pack()
-run_btn.pack()
 coordenadas.pack()
-
-
-# Repeat after an interval to capture continiously
+run_btn.pack()
 
 win.mainloop()
