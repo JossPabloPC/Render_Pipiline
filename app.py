f56be0265ea8afab806b1b00ac27297a1c7b6e8e
JossPabloPC <51423507+JossPabloPC@users.noreply.github.com>
2022-08-29 20:17:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,45 @@
 # Import required Libraries
-from tkinter import *
+import tkinter as tk
 from PIL import Image, ImageTk
 import numpy as np
 import cv2
 
-# Create an instance of TKinter Window or frame
-win= Tk()
+#Functions
+def array2Window(arr):
+      mylabel = tk.Label(text="clicked")
+      mylabel.pack()     
+#      img = Image.fromarray(arr)
+#
+#      # Convert image to PhotoImage
+#      imgtk             = ImageTk.PhotoImage(image = img)
+#      output_image      = tk.Label(image=imgtk)
+#      output_image.image= imgtk
+#      output_image.pack()
 
-# Set the size of the window
-win.geometry("700x350")# Create a Label to capture the Video frames
-label =Label(win)
-label.grid(row=0, column=0)
+# Create an instance of TKinter Window or frame
+win= tk.Tk()
+win.geometry("1080x720")# Create a Label to capture the Video frames
+
+# Output image
 cap= np.zeros((300,300,3), dtype=np.uint8)
 
-# Define function to show frame
-def show_frames():
-      # Get the latest frame and convert into Image
-      
-      img = Image.fromarray(cap)
+#Frames
+coordenadas = tk.Frame()
 
-      # Convert image to PhotoImage
-      imgtk = ImageTk.PhotoImage(image = img)
-      label.imgtk = imgtk
-      label.configure(image=imgtk)
+#Widgets
+greeting = tk.Label(text="Welcome to Python Render Pipeline", fg="white", bg="black",width=50, height=3)
+entryX      = tk.Entry(master=coordenadas)
+entryY       = tk.Entry(master=coordenadas)
+run_btn = tk.Button(command=array2Window(cap), text= "Run")
+
+#Packing
+greeting.pack()
+entryX.pack(side=tk.LEFT)
+entryY.pack()
+run_btn.pack()
+coordenadas.pack()
+
 
 # Repeat after an interval to capture continiously
 
-show_frames()
 win.mainloop()
